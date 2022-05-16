5d38cfabae2840847f2df149486f821bf88b6512
micheleambrosio <micheleambrosio.dev@gmail.com>
2022-05-16 00:05:20 -0300

    EXTRA: adiciona rotacao no eixo Z para adicionar um efeito

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -8,12 +8,35 @@ function handleMouseLeave() {
   document.body.id = '';
 }
 
+function getPreviousAndNextCard(currentSelectedCard) {
+  const MAX_CARDS = 3;
+  let previousCard = currentSelectedCard - 1;
+  let nextCard = currentSelectedCard + 1;
+
+  if (currentSelectedCard === MAX_CARDS) {
+    nextCard = 1;
+  }
+
+  if (currentSelectedCard === 1) {
+    previousCard = 3;
+  }
+
+  return {
+    previous: previousCard,
+    next: nextCard
+  }
+}
+
+function changeRotateZStyle(transform, rotateZDeg) {
+  return transform.replace(/rotateZ\((-?\d+deg)\)/i, `rotateZ(${rotateZDeg}deg)`);
+}
+
 function handleControllerButtonClick() {
+  const currentSelectedCard = Number(this.id);
   const carousel = document.querySelector('.s-cards-carousel');
   const transform = carousel.style.transform;
-  const rotateY = transform.match(/rotateY\((\d+deg)\)/i);
-  // const rotateYDeg = Number(rotateY[1].replace('deg', '')) + 120;
-  const rotateYDeg = 120 * Number(this.id);
+  const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+  const rotateYDeg = -120 * (currentSelectedCard - 1);
   const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
 
   carousel.style.transform = newTransform;
@@ -21,6 +44,15 @@ function handleControllerButtonClick() {
   const activeButtonElement = document.querySelector('.s-controller__button--active');
   activeButtonElement.classList.remove('s-controller__button--active');
   this.classList.add('s-controller__button--active');
+
+  // Rotation cards effect
+
+  const cardsElements = document.getElementsByClassName('s-card');
+  const cards = getPreviousAndNextCard(currentSelectedCard);
+
+  cardsElements[cards.next - 1].style.transform = changeRotateZStyle(cardsElements[cards.next - 1].style.transform, -10);
+  cardsElements[cards.previous - 1].style.transform = changeRotateZStyle(cardsElements[cards.previous - 1].style.transform, 10);
+  cardsElements[currentSelectedCard - 1].style.transform = changeRotateZStyle(cardsElements[currentSelectedCard - 1].style.transform, 0);
 }
 
 function addEventListenersToCards() {

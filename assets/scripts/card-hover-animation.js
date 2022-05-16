ce1704da129a2c9c8196508f77eef68e7abc9f20
micheleambrosio <micheleambrosio.dev@gmail.com>
2022-05-15 22:49:52 -0300

    Aplica carrossel 3D

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -8,6 +8,21 @@ function handleMouseLeave() {
   document.body.id = '';
 }
 
+function handleControllerButtonClick() {
+  const carousel = document.querySelector('.s-cards-carousel');
+  const transform = carousel.style.transform;
+  const rotateY = transform.match(/rotateY\((\d+deg)\)/i);
+  // const rotateYDeg = Number(rotateY[1].replace('deg', '')) + 120;
+  const rotateYDeg = 120 * Number(this.id);
+  const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+
+  carousel.style.transform = newTransform;
+
+  const activeButtonElement = document.querySelector('.s-controller__button--active');
+  activeButtonElement.classList.remove('s-controller__button--active');
+  this.classList.add('s-controller__button--active');
+}
+
 function addEventListenersToCards() {
   const cardElements = document.getElementsByClassName('s-card');
   
@@ -18,4 +33,14 @@ function addEventListenersToCards() {
   }
 }
 
+function addEventListenersToCarouselButtons() {
+  const carouselButtons = document.getElementsByClassName('s-controller__button');
+  
+  for (let index = 0; index < carouselButtons.length; index++) {
+    const card = carouselButtons[index];
+    card.addEventListener('click', handleControllerButtonClick);
+  } 
+}
+
 document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
+document.addEventListener("DOMContentLoaded", addEventListenersToCarouselButtons, false);
